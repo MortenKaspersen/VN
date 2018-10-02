341c9ae0d38dce5b86403992f43aa384e30aafd9
MortenKaspersen <mortenps@hotmail.com>
2018-10-02 20:30:00 +0200

    Add project files.

---
+++ VN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VN.MvcApplication" Language="C#" %>
---
+++ VN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VN.Startup))]
+namespace VN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
